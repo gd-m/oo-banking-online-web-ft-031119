f659a20f5e0d764e8ec2f1fa538f98244ab99871
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-27 23:26:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,12 @@
 class Transfer
   # your code here
   attr_accessor :sender,:receiver, :amount
-  
+
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @amount = amount    
-    
+    @amount = amount
+
   end
-  
+
 end
